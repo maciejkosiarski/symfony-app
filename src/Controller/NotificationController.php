c571d459aa1e3d701c3516e7dc5c3a5e33309788
maciejkosiarski <maciek.kosiarski@gmail.com>
2018-03-16 08:21:59 +0100

    crud generated based on notification entity

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Notification;
+use App\Form\NotificationType;
+use App\Repository\NotificationRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/notification")
+ * @Security("has_role('ROLE_USER')")
+ */
+class NotificationController extends Controller
+{
+    /**
+     * @Route("/", name="notification_index", methods="GET")
+	 * @param NotificationRepository $notificationRepository
+	 * @return Response
+     */
+    public function index(NotificationRepository $notificationRepository): Response
+    {
+        return $this->render('notification/index.html.twig', ['notifications' => $notificationRepository->findByUser($this->getUser()->getId())]);
+    }
+
+    /**
+     * @Route("/new", name="notification_new", methods="GET|POST")
+	 * @param Request $request
+	 * @return Response
+	 * @throws \App\Exception\InvalidNotificationTypeException
+	 * @throws \ReflectionException
+	 */
+    public function new(Request $request): Response
+    {
+        $notification = new Notification($this->getUser(), Notification::TYPE_EMAIL);
+        $form = $this->createForm(NotificationType::class, $notification);
+        $form->handleRequest($request);
+		dump($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($notification);
+            $em->flush();
+
+            return $this->redirectToRoute('notification_index');
+        }
+
+        return $this->render('notification/new.html.twig', [
+            'notification' => $notification,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="notification_show", methods="GET")
+	 * @param Notification $notification
+	 * @return Response
+	 */
+    public function show(Notification $notification): Response
+    {
+        return $this->render('notification/show.html.twig', ['notification' => $notification]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="notification_edit", methods="GET|POST")
+	 * @param Request      $request
+	 * @param Notification $notification
+	 * @return Response
+	 */
+    public function edit(Request $request, Notification $notification): Response
+    {
+        $form = $this->createForm(NotificationType::class, $notification);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('notification_edit', ['id' => $notification->getId()]);
+        }
+
+        return $this->render('notification/edit.html.twig', [
+            'notification' => $notification,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="notification_delete", methods="DELETE")
+	 * @param Request      $request
+	 * @param Notification $notification
+	 * @return Response
+	 */
+    public function delete(Request $request, Notification $notification): Response
+    {
+        if (!$this->isCsrfTokenValid('delete'.$notification->getId(), $request->request->get('_token'))) {
+            return $this->redirectToRoute('notification_index');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($notification);
+        $em->flush();
+
+        return $this->redirectToRoute('notification_index');
+    }
+}
---
+++ src/Form/NotificationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Notification;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NotificationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type', IntegerType::class)
+            ->add('message', TextType::class)
+            ->add('dueDate', DateTimeType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Notification::class,
+        ]);
+    }
+}
