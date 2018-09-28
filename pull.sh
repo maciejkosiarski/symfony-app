ff075a3307f35abe50ff86e0a0237b8f727cca95
maciejkosiarski <maciek.kosiarski@gmail.com>
2018-09-28 21:01:33 +0200

    pulling bash script added

---
+++ pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull &&
+composer install &&
+php run cache:clear &&
+php run cache:warmup &&
+php run doctrine:migrations:migrate &&
+php run cache:clear &&
+hp run cache:warmup &&
