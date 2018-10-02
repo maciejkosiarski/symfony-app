f62c9984b69f1ad97bb723452fc5c9f4b68a1929
maciejkosiarski <maciek.kosiarski@gmail.com>
2018-10-02 22:43:34 +0200

    share finder test modified

--- tests/Service/ShareFinderTest.php
+++ tests/Service/ShareFinderTest.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Service;
 
+use App\Entity\Company;
 use App\Entity\CompanyShare;
+use App\Entity\CompanySource;
+use App\Exception\StockExchange\AllSourceFailedException;
 use App\Service\ShareFinder;
-use GuzzleHttp\Exception\ClientException;
 use PHPUnit\Framework\TestCase;
 
 class ShareFinderTest extends TestCase
@@ -13,36 +17,80 @@ class ShareFinderTest extends TestCase
 
     public function __construct(?string $name = null, array $data = [], string $dataName = '')
     {
-        parent::__construct($name, $data, $dataName);
         $this->finder = new ShareFinder();
+
+        parent::__construct($name, $data, $dataName);
     }
 
     /**
      * @dataProvider provideCompanies
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws AllSourceFailedException
      */
-	public function testCorrectFind(string $company)
-	{
+    public function testCorrectFind(Company $company): void
+    {
         $share = $this->finder->find($company);
 
         $this->assertInstanceOf(CompanyShare::class, $share);
-	}
+    }
 
     /**
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws AllSourceFailedException
      */
-    public function testFindException()
+    public function testFindException(): void
     {
-        $this->expectException(ClientException::class);
-        $this->finder->find('UNDEFINED');
-	}
+        $this->expectException(AllSourceFailedException::class);
+        $this->finder->find($this->getFalseCompany());
+    }
 
     public function provideCompanies(): array
     {
-        return [
-            ['PGE'],
-            ['CDPROJEKT'],
-            ['MBANK'],
+        $sources = [
+            [
+                'path' => 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=PGE',
+                'priceSelector' => '#boxProfilHeader > .boxHeader > .textNowrap > .profilLast',
+            ],
+            [
+                'path' => 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=CDPROJEKT',
+                'priceSelector' => '#boxProfilHeader > .boxHeader > .textNowrap > .profilLast',
+            ],
+            [
+                'path' => 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=CIGAMES',
+                'priceSelector' => '#boxProfilHeader > .boxHeader > .textNowrap > .profilLast',
+            ],
         ];
+
+        $companies = [];
+
+        foreach ($sources as $source) {
+            array_push($companies, [$this->getCompany($source['path'], $source['priceSelector'])]);
+        }
+
+        return $companies;
+    }
+
+    private function getCompany($path, $priceSelector): Company
+    {
+        $source = new CompanySource();
+        $source->setPath($path);
+        $source->setPriceSelector($priceSelector);
+
+        $company = new Company();
+        $company->setName('Test company');
+        $company->addToSources($source);
+
+        return $company;
+    }
+
+    private function getFalseCompany(): Company
+    {
+        $source = new CompanySource();
+        $source->setPath('false_path');
+        $source->setPriceSelector('false_selector');
+
+        $company = new Company();
+        $company->setName('Test company');
+        $company->addToSources($source);
+
+        return $company;
     }
 }
