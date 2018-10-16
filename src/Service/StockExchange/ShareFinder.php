5e855e9a0b79fd14c8d725d64e67a5413c98e2d1
maciejkosiarski <maciek.kosiarski@gmail.com>
2018-10-16 19:11:45 +0200

    share finder refactored

--- src/Service/StockExchange/ShareFinder.php
+++ src/Service/StockExchange/ShareFinder.php
@@ -7,17 +7,23 @@ namespace App\Service\StockExchange;
 use App\Entity\Company;
 use App\Entity\CompanyShare;
 use App\Entity\CompanySource;
+use App\Event\StockExchange\ShareFoundExceptionEvent;
 use App\Exception\StockExchange\AllSourceFailedException;
+use App\Exception\StockExchange\SourceFailedException;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class ShareFinder
 {
     private $clientHttp;
 
+    private $dispatcher;
+
     public function __construct()
     {
         $this->clientHttp = new Client();
+        $this->dispatcher = new EventDispatcher();
     }
 
     /**
@@ -25,33 +31,54 @@ class ShareFinder
      */
     public function find(Company $company): CompanyShare
     {
+
         /** @var CompanySource $source */
         foreach ($company->getSources() as $source) {
             try {
-                $html = $this->clientHttp
-                    ->request('GET', $source->getPath());
-
-                $crawler = new Crawler();
-                $crawler->add($html->getBody()->getContents());
-
-                $price = $crawler
-                    ->filter($source->getPriceSelector())
-                    ->getNode(0)
-                    ->nodeValue;
-
-                $price = str_replace(',', '.', preg_replace("/[^0-9,.]/", "", $price));
-
                 $share = new CompanyShare();
                 $share->setCompany($company);
-                $share->setPrice((float) $price);
+                $share->setPrice($this->findPrice($source));
 
                 return $share;
             } catch (\GuzzleHttp\Exception\GuzzleException | \Exception $e) {
-                /** todo log something */
-                continue;
+                $this->dispatchFoundExceptionEvent($e);
             }
         }
 
         throw new AllSourceFailedException($company);
+    }
+
+    /**
+     * @throws SourceFailedException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function findPrice(CompanySource $source): float
+    {
+        $html = $this->clientHttp
+            ->request('GET', $source->getPath());
+
+        $crawler = new Crawler();
+        $crawler->add($html->getBody()->getContents());
+
+        $price = $crawler
+            ->filter($source->getPriceSelector())
+            ->getNode(0)
+            ->nodeValue;
+
+        $price = str_replace(',', '.', preg_replace("/[^0-9,.]/", "", $price));
+
+        if (is_numeric($price)) {
+            return (float) $price;
+        }
+
+        throw new SourceFailedException($source);
+    }
+
+    private function dispatchFoundExceptionEvent(\Exception $e): void
+    {
+        $this->dispatcher->dispatch(
+            ShareFoundExceptionEvent::NAME,
+            new ShareFoundExceptionEvent($e)
+        );
     }
 }
